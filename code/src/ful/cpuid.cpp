e1709fb075af55034d79873f73a301dbfbc9873b
Simon Hössjer <simon_hossjer@hotmail.com>
2021-05-06 21:41:37 +0200

    Prototype runtime probing of cpuid :probing_cane:

--- code/src/ful/cpuid.cpp
+++ code/src/ful/cpuid.cpp
@@ -1,3 +1,6 @@
+#if defined(FUL_FPTR)
+
+#include "ful/compiler.hpp"
 #include "ful/cpuid.hpp"
 
 #if defined(_MSC_VER)
@@ -6,12 +9,7 @@
 # include <cpuid.h>
 #endif
 
-#define CHECK_BIT(reg, bit) (reg & (1 << bit))
-
-namespace ful
-{
-	unsigned int cpuid_feature_cache = 0;
-}
+#define CHECK_BIT(reg, bit) (reg & (1u << bit))
 
 namespace
 {
@@ -33,56 +31,61 @@ namespace
 # error Missing implementation!
 #endif
 	}
-
-	struct static_hack_type
-	{
-		static_hack_type()
-		{
-			// https://software.intel.com/content/www/us/en/develop/download/intel-architecture-instruction-set-extensions-programming-reference.html
-			// https://www.amd.com/system/files/TechDocs/24594.pdf
-			reg_type info[4];
-
-			get_cpuid(0, info);
-			const unsigned int max_id = info[0];
-
-			if (max_id >= 1)
-			{
-				get_cpuid(1, info);
-				const int eax = info[0];
-				const int model = ((static_cast<unsigned int>(eax) >> 12) & 0xf0) | ((static_cast<unsigned int>(eax) >> 4) & 0x0f);
-				const int family = ((static_cast<unsigned int>(eax) >> 16) & 0xf0) | ((static_cast<unsigned int>(eax) >> 8) & 0x0f);
-				const int ecx = info[2];
-				const int edx = info[3];
-				if (CHECK_BIT(ecx, 0)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::SSE3);
-				if (CHECK_BIT(ecx, 9)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::SSSE3);
-				if (CHECK_BIT(ecx, 19)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::SSE4_1);
-				if (CHECK_BIT(ecx, 20)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::SSE4_2);
-				if (CHECK_BIT(ecx, 23)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::POPCNT);
-				if (CHECK_BIT(ecx, 28)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::AVX);
-				// if (CHECK_BIT(edx, 23)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::MMX);
-				if (CHECK_BIT(edx, 25)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::SSE);
-				if (CHECK_BIT(edx, 26)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::SSE2);
-			}
-
-			if (max_id >= 7)
-			{
-				get_cpuid(7, info);
-				const int ebx = info[1];
-				if (CHECK_BIT(ebx, 3)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::BMI);
-				if (CHECK_BIT(ebx, 5)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::AVX2);
-				if (CHECK_BIT(ebx, 8)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::BMI2);
-			}
-
-			get_cpuid(0x80000000, info);
-			const unsigned int max_ext_id = info[0];
-
-			if (max_ext_id >= 0x80000001)
-			{
-				get_cpuid(0x80000001, info);
-				const int ecx = info[2];
-				if (CHECK_BIT(ecx, 5)) ful::cpuid_feature_cache |= static_cast<unsigned int>(ful::cpuid_feature::LZCNT);
-			}
-		}
-	}
-	static_hack;
 }
+
+namespace ful
+{
+	unsigned int cpuid_feature_cache = 0;
+
+	void cpuid_init()
+	{
+		if (cpuid_feature_cache != 0)
+			return;
+
+		// https://software.intel.com/content/www/us/en/develop/download/intel-architecture-instruction-set-extensions-programming-reference.html
+		// https://www.amd.com/system/files/TechDocs/24594.pdf
+		reg_type info[4];
+
+		get_cpuid(0, info);
+		const unsigned int max_id = info[0];
+
+		if (max_id >= 1)
+		{
+			get_cpuid(1, info);
+			const int ecx = info[2];
+			const int edx = info[3];
+			if (CHECK_BIT(ecx, 0)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::SSE3);
+			if (CHECK_BIT(ecx, 9)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::SSSE3);
+			if (CHECK_BIT(ecx, 19)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::SSE4_1);
+			if (CHECK_BIT(ecx, 20)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::SSE4_2);
+			if (CHECK_BIT(ecx, 23)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::POPCNT);
+			if (CHECK_BIT(ecx, 28)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::AVX);
+			// if (CHECK_BIT(edx, 23)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::MMX);
+			if (CHECK_BIT(edx, 25)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::SSE);
+			if (CHECK_BIT(edx, 26)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::SSE2);
+		}
+
+		if (max_id >= 7)
+		{
+			get_cpuid(7, info);
+			const int ebx = info[1];
+			if (CHECK_BIT(ebx, 3)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::BMI);
+			if (CHECK_BIT(ebx, 5)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::AVX2);
+			if (CHECK_BIT(ebx, 8)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::BMI2);
+		}
+
+		get_cpuid(0x80000000, info);
+		const unsigned int max_ext_id = info[0];
+
+		if (max_ext_id >= 0x80000001)
+		{
+			get_cpuid(0x80000001, info);
+			const int ecx = info[2];
+			if (CHECK_BIT(ecx, 5)) cpuid_feature_cache |= static_cast<unsigned int>(cpuid_feature::LZCNT);
+		}
+
+		cpuid_feature_cache |= 0x80000000;
+	}
+}
+
+#endif
--- code/src/ful/string.cpp
+++ code/src/ful/string.cpp
@@ -1,43 +1,78 @@
+#if defined(FUL_IFUNC) || defined(FUL_FPTR)
+
+#include "ful/compiler.hpp"
 #include "ful/cpuid.hpp"
 #include "ful/string.hpp"
 
 using namespace ful;
 using namespace ful::detail;
 
+#if defined(FUL_IFUNC)
+# define ful_cpucall(stem, func, ...) func
+# define ful_cpuinit() __builtin_cpu_init()
+# define ful_cpucheck(name, value) __builtin_cpu_supports(name)
+# define ful_function(stem) stem##_type stem __attribute__((ifunc ("resolve_" #stem)))
+# define ful_resolve(stem) (* resolve_##stem())
+#elif defined(FUL_FPTR)
+# define ful_cpucall(stem, func, ...) stem = func, func(__VA_ARGS__)
+# define ful_cpuinit() ful::cpuid_init()
+# define ful_cpucheck(name, value) ful::cpuid_supports(cpuid_feature::value)
+# define ful_function(stem) stem##_type * stem = resolve_##stem
+# define ful_resolve(stem) resolve_##stem
+#endif
+
 namespace
 {
-	bool equal_cstr_dispatch_init(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2);
-	bool less_cstr_dispatch_init(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2);
+	using equal_cstr_type = bool(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2);
+	using less_cstr_type = bool(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2);
+
+#if defined(FUL_FPTR)
+	equal_cstr_type resolve_equal_cstr;
+	less_cstr_type resolve_less_cstr;
+#endif
 }
 
 namespace ful
 {
 	namespace detail
 	{
-		bool (* equal_cstr_dispatch)(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2) = equal_cstr_dispatch_init;
-		bool (* less_cstr_dispatch)(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2) = less_cstr_dispatch_init;
+		ful_function(equal_cstr);
+		ful_function(less_cstr);
 	}
 }
 
 namespace
 {
-	bool equal_cstr_dispatch_init(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2)
+#if defined(FUL_IFUNC)
+	extern "C"
 	{
-		equal_cstr_dispatch = cpuid_switch(
-			cpuid_feature::AVX2, equal_cstr_avx2,
-			cpuid_feature::SSE2, equal_cstr_sse2,
-			equal_cstr_none);
+#endif
 
-		return equal_cstr_dispatch(beg1, end1, beg2);
+		bool ful_resolve(equal_cstr)(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2)
+		{
+			ful_cpuinit();
+			if (ful_cpucheck("avx2", AVX2))
+				return ful_cpucall(equal_cstr, equal_cstr_avx2, beg1, end1, beg2);
+			//else if (ful_cpucheck("sse2", SSE2))
+			//	return ful_cpucall(equal_cstr, equal_cstr_sse2, beg1, end1, beg2);
+			else
+				return ful_cpucall(equal_cstr, equal_cstr_none, beg1, end1, beg2);
+		}
+
+		bool ful_resolve(less_cstr)(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2)
+		{
+			ful_cpuinit();
+			if (ful_cpucheck("avx2", AVX2))
+				return ful_cpucall(less_cstr, less_cstr_avx2, beg1, end1, beg2);
+			//else if (ful_cpucheck("sse2", SSE2))
+			//	return ful_cpucall(less_cstr, less_cstr_sse2, beg1, end1, beg2);
+			else
+				return ful_cpucall(less_cstr, less_cstr_none, beg1, end1, beg2);
+		}
+
+#if defined(FUL_IFUNC)
 	}
-
-	bool less_cstr_dispatch_init(const unit_utf8 * beg1, const unit_utf8 * end1, const unit_utf8 * beg2)
-	{
-		less_cstr_dispatch = cpuid_switch(
-			cpuid_feature::AVX2, less_cstr_avx2,
-			cpuid_feature::SSE2, less_cstr_sse2,
-			less_cstr_none);
-
-		return less_cstr_dispatch(beg1, end1, beg2);
-	}
+#endif
 }
+
+#endif
