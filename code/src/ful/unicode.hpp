f1e6b89c56a600c769afaeadbcbed1dc3447974d
Simon Hössjer <simon_hossjer@hotmail.com>
2021-05-09 00:08:28 +0200

    Benchmark unicode conversions :recycle:

--- code/src/ful/unicode.hpp
+++ code/src/ful/unicode.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+// #include "ful/stdint.hpp"
+
 namespace ful
 {
 	using unit_utf8 = char;
@@ -7,4 +9,45 @@ namespace ful
 #if defined(_MSC_VER)
 	using unit_utfw = wchar_t;
 #endif
+
+	inline unsigned int point_size(const unit_utf8 * s)
+	{
+		if (static_cast<signed char>(*s) >= 0)
+			return 1;
+
+		if ((static_cast<signed char>(*s) & 0x20) == 0)
+			return 2;
+
+		if ((static_cast<signed char>(*s) & 0x10) == 0)
+			return 3;
+
+		return 4;
+	}
+
+	inline const unit_utf8 * point_next(const unit_utf8 * s) { return s + point_size(s); }
+
+	inline const unit_utf8 * point_next(const unit_utf8 * s, usize n)
+	{
+		extern const unit_utf8 * (* point_next_dispatch)(const unit_utf8 * s, usize n);
+
+		return point_next_dispatch(s, n);
+	}
+
+	inline const unit_utf8 * point_prev(const unit_utf8 * s)
+	{
+		--s;
+		if ((*s & 0x80) == 0)
+			return s;
+
+		--s;
+		if (*s & 0x40)
+			return s;
+
+		--s;
+		if (*s & 0x40)
+			return s;
+
+		--s;
+		return s;
+	}
 }
