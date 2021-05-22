5d52080d28c14c0546b9d4582f41f3c264db5482
Simon Hössjer <simon_hossjer@hotmail.com>
2021-05-23 01:41:44 +0200

    Add generic attribute target :genie:

--- code/src/ful/unicode_none.hpp
+++ code/src/ful/unicode_none.hpp
@@ -6,7 +6,8 @@ namespace ful
 {
 	namespace detail
 	{
-		inline usize point_count_none(const unit_utf8 * beg, const unit_utf8 * end)
+		ful_generic() inline
+		usize point_count_none(const unit_utf8 * beg, const unit_utf8 * end)
 		{
 			usize count = 0;
 
@@ -15,27 +16,26 @@ namespace ful
 				count++;
 				beg = point_next(beg);
 			}
-
 			return count;
 		}
 
-		inline const unit_utf8 * point_next_none(const unit_utf8 * s, usize n)
+		ful_generic() inline
+		const unit_utf8 * point_next_none(const unit_utf8 * s, usize n)
 		{
 			while (0 < n)
 			{
 				s = point_next(s);
 			}
-
 			return s;
 		}
 
-		inline const unit_utf8 * point_prev_none(const unit_utf8 * s, usize n)
+		ful_generic() inline
+		const unit_utf8 * point_prev_none(const unit_utf8 * s, usize n)
 		{
 			while (0 < n)
 			{
 				s = point_prev(s);
 			}
-
 			return s;
 		}
 	}
