05b7ab0a2f3709189482b1f99d857a941f13f5a9
Simon Hössjer <simon_hossjer@hotmail.com>
2021-06-13 01:08:43 +0200

    Improve point utils :imp:
    Extracting the point size when stepping to the next point is actually
    slightly slower than assuming the next character exist and check if it
    is the beginning of a new code point.

--- code/src/ful/point_utils.hpp
+++ code/src/ful/point_utils.hpp
@@ -6,32 +6,48 @@ namespace ful
 {
 	inline unsigned int point_size(const unit_utf8 * s)
 	{
-		if (static_cast<signed char>(*s) >= 0)
+		if (static_cast<sint8>(*s) >= 0)
 			return 1;
 
-		if ((static_cast<signed char>(*s) & 0x20) == 0)
+		if ((static_cast<sint8>(*s) & 0x20) == 0)
 			return 2;
 
-		if ((static_cast<signed char>(*s) & 0x10) == 0)
+		if ((static_cast<sint8>(*s) & 0x10) == 0)
 			return 3;
 
 		return 4;
 	}
 
-	inline const unit_utf8 * point_next(const unit_utf8 * s) { return s + point_size(s); }
+	inline const unit_utf8 * point_next(const unit_utf8 * s)
+	{
+		++s;
+		if (static_cast<sint8>(*s) > -65)
+			return s;
+
+		++s;
+		if (static_cast<sint8>(*s) > -65)
+			return s;
+
+		++s;
+		if (static_cast<sint8>(*s) > -65)
+			return s;
+
+		++s;
+		return s;
+	}
 
 	inline const unit_utf8 * point_prev(const unit_utf8 * s)
 	{
 		--s;
-		if ((*s & 0x80) == 0)
+		if (static_cast<sint8>(*s) > -65)
 			return s;
 
 		--s;
-		if (*s & 0x40)
+		if (static_cast<sint8>(*s) > -65)
 			return s;
 
 		--s;
-		if (*s & 0x40)
+		if (static_cast<sint8>(*s) > -65)
 			return s;
 
 		--s;
